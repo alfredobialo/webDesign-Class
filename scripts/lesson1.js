7f4e3dba93297ceea8f1c9103f0c8361afee960a
Alfred Obialo <alfredcsdinc@gmail.com>
2023-11-18 01:39:27 +0100

    Layout Design

---
+++ scripts/lesson1.js
@@ -0,0 +1,17 @@
+﻿let userNameElement = document.getElementById("user-name");
+console.log(userNameElement);
+userNameElement.onmouseover = (evt) =>{
+  userNameElement.style.color ="green";
+};
+userNameElement.onmouseout = (evt) =>{
+    userNameElement.style.color ="inherit";
+};
+
+let userNameInput =  prompt("Welcome to our site\n can we get your Name Please:","");
+if(userNameInput === "")
+{
+    //alert("You didn't supply a valid name, are you Mad?");
+}
+else{
+    userNameElement.innerText = userNameInput;
+}
